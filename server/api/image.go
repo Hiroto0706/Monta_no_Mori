88b171148836366c4908f2eeda5b295c475fbd70
Hiroto0706 <mirunina2366@icloud.com>
2023-10-21 19:04:40 +0900

    add : localhost:8080にアクセスするとHelloWorldを返すコード

---
+++ server/api/image.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (server *Server) HelloWorld(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Hello World!",
+	})
+}
