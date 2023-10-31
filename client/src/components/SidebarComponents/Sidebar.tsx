49a401bfbbda284362e4d77c3179f6d3f6ede383
Hiroto0706 <mirunina2366@icloud.com>
2023-11-01 01:39:19 +0900

    add : サイドバーの内容作成

--- client/src/components/SidebarComponents/Sidebar.tsx
+++ client/src/components/SidebarComponents/Sidebar.tsx
@@ -9,18 +9,60 @@ export default function Sidebar() {
         <div className="sidebar__inner">
           <div className="sidebar__inner__type">
             <h3>Type</h3>
-            <span>テスト</span>
-            <span>テスト</span>
+            <a href="">
+              テスト
+              <div>
+                <img src="public/pc-img.png" />
+              </div>
+            </a>
+            <a href="">
+              テスト
+              <div>
+                <img src="public/pc-img.png" />
+              </div>
+            </a>
+            <a href="">
+              テスト
+              <div>
+                <img src="public/pc-img.png" />
+              </div>
+            </a>
+            <a href="">
+              テスト
+              <div>
+                <img src="public/pc-img.png" />
+              </div>
+            </a>
           </div>
 
           <div className="sidebar__inner__category">
             <h3>Category</h3>
-            <span>テスト</span>
-            <span>テスト</span>
-            <span>テスト</span>
-            <span>テスト</span>
-            <span>テスト</span>
-            <span>テスト</span>
+            <a href="">#テスト1</a>
+            <a href="">#テスト2</a>
+            <a href="">#テスト3</a>
+            <a href="">#テスト4</a>
+            <a href="">#テスト5</a>
+            <a href="">#テスト6</a>
+            <a href="">#テスト7</a>
+            <a href="">#テスト8</a>
+            <a href="">#テスト9</a>
+            <a href="">#テスト10</a>
+            <a href="">#テスト11</a>
+            <a href="">#テスト12</a>
+            <a href="">#テスト13</a>
+            <a href="">#テスト14</a>
+            <a href="">#テスト15</a>
+            <a href="">#テスト16</a>
+            <a href="">#テスト17</a>
+            <a href="">#テスト18</a>
+            <a href="">#テスト19</a>
+            <a href="">#テスト20</a>
+            <a href="">#テスト21</a>
+            <a href="">#テスト22</a>
+            <a href="">#テスト23</a>
+            <a href="">#テスト24</a>
+            <a href="">#テスト25</a>
+            <a href="">#テスト26</a>
           </div>
         </div>
       </div>
