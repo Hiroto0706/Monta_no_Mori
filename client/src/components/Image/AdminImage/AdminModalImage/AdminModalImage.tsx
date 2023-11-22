c3fee016c85440f416d8a641616099a5d224a701
Hiroto0706 <mirunina2366@icloud.com>
2023-11-23 07:32:39 +0900

    stgとして

--- client/src/components/Image/AdminImage/AdminModalImage/AdminModalImage.tsx
+++ client/src/components/Image/AdminImage/AdminModalImage/AdminModalImage.tsx
@@ -1,32 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import AdminCategoryModal from "./AdminCategoryModal/AdminCategoryModal";
 import { EllipsisText } from "../../../Sidebar/UserSidebar/Sidebar";
+import {
+  Image,
+  Category,
+  Type,
+} from "./../../../../pages/Admin/AdminImage/AdminImage";
+import { fetchTypes } from "../../../../pages/Admin/AdminType/AdminType";
 
 import "./AdminModalImage.css";
 
 interface ModalImageProps {
-  src: string;
-  title: string;
   toggleOpenModal: () => void;
 }
 
-const AdminModalImage: React.FC<ModalImageProps> = ({
-  src,
+const AdminModalImage: React.FC<Image & ModalImageProps> = ({
   title,
+  src,
+  type,
+  categories,
   toggleOpenModal,
 }) => {
   const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false);
+  const [types, setTypes] = useState<Type[]>([]);
 
   const handleCategoryModal = () => {
     setIsCategoryModalOpen(!isCategoryModalOpen);
   };
+  const closeCategoryModal = () => {
+    setIsCategoryModalOpen(false);
+  };
+
+  useEffect(() => {
+    fetchTypes(setTypes);
+  }, []);
 
   return (
     <div className="modal-image__overlay" onClick={toggleOpenModal}>
       <div
         className="modal-image__content"
-        onClick={(e) => e.stopPropagation()}
+        onClick={(e) => {
+          e.stopPropagation();
+          closeCategoryModal();
+        }}
       >
         <button onClick={toggleOpenModal} className="cancel">
           <img src="/cancel-icon.png" />
@@ -40,63 +57,53 @@ const AdminModalImage: React.FC<ModalImageProps> = ({
         </div>
 
         <div className="modal-image__content__desc">
-          <div className="type">
-            <h3>Title</h3>
-            <input value={title} placeholder="title" />
-          </div>
+          <div>
+            <div className="type">
+              <h3>Title</h3>
+              <input value={title} placeholder="title" />
+            </div>
 
-          <div className="type">
-            <h3>Type</h3>
-            <label className="selectbox">
-              <select>
-                <option>-- Select Type --</option>
-                <option>optionの例1</option>
-                <option>optionの例2</option>
-                <option>optionの例3</option>
-              </select>
-            </label>
-          </div>
+            <div className="type">
+              <h3>Type</h3>
+              <label className="selectbox">
+                <select value={type.id}>
+                  <option>-- Select Type --</option>
+                  {types.map((t) => (
+                    <option key={t.id} value={t.id}>
+                      {t.name}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
 
-          <div className="category">
-            <h3>Category</h3>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <a href="" className="category-link">
-              #
-              <EllipsisText text="テストテストテスト" maxLength={100} />
-            </a>
-            <span className="category-link add" onClick={handleCategoryModal}>
-              {isCategoryModalOpen ? "CLOSE" : "+ ADD"}
-              {isCategoryModalOpen && (
-                <AdminCategoryModal onClose={handleCategoryModal} />
-              )}
-            </span>
+            <div className="category">
+              <h3>Category</h3>
+              {categories?.map((category: Category) => (
+                <span key={category.id} className="category-link">
+                  # <EllipsisText text={category.name} maxLength={100} />
+                  <img
+                    src="/cancel-icon.png"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      console.log("deselect category");
+                    }}
+                  />
+                </span>
+              ))}
+              <span
+                className="category-link add"
+                onClick={(e) => {
+                  handleCategoryModal();
+                  e.stopPropagation();
+                }}
+              >
+                {isCategoryModalOpen ? "CLOSE" : "+ ADD"}
+                {isCategoryModalOpen && (
+                  <AdminCategoryModal onClose={handleCategoryModal} />
+                )}
+              </span>
+            </div>
           </div>
 
           <div className="modal-image__content__desc__button">
